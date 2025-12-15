df394236d68ee5129e10b861587d06235d7363b6
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-12-15 17:53:49 +0100

    log the request

--- clinics/patient/src/main/java/com/microservices/patient/controller/PatientController.java
+++ clinics/patient/src/main/java/com/microservices/patient/controller/PatientController.java
@@ -1,19 +1,26 @@
 package com.microservices.patient.controller;
 
 import com.microservices.patient.service.PatientService;
+
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
+@Slf4j
 @RestController
 public class PatientController {
 
     @Autowired
     private PatientService patientService;
 
-    @GetMapping(path = "/search/{patientName}")
-    public String searchPatientName(@PathVariable("patientName") String patientName) {
-        return this.patientService.getPatientName(patientName);
+    @GetMapping("/search/{patientName}")
+    public String searchPatientName(@PathVariable String patientName) {
+        log.info("Searching for patient with name {} - {}", patientName, UUID.randomUUID());
+        return patientService.getPatientName(patientName);
     }
 }
