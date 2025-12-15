f74d1123e7998361f729dd781e775d6d5987d626
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-12-15 16:01:43 +0100

    get PatientController endpoint

--- clinics/patient/src/main/java/com/microservices/patient/controller/PatientController.java
+++ clinics/patient/src/main/java/com/microservices/patient/controller/PatientController.java
@@ -1,8 +1,19 @@
 package com.microservices.patient.controller;
 
+import com.microservices.patient.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PatientController {
 
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping(path = "/search/{patientName}")
+    public String searchPatientName(@PathVariable("patientName") String patientName) {
+        return this.patientService.getPatientName(patientName);
+    }
 }
