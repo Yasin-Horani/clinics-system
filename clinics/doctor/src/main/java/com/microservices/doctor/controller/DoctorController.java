60228e5a2b9bf2aae7681d21f4dbc6b6a2d9058f
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-12-15 16:51:48 +0100

    make comunication between patient and doctor with using RestTemplate

--- clinics/doctor/src/main/java/com/microservices/doctor/controller/DoctorController.java
+++ clinics/doctor/src/main/java/com/microservices/doctor/controller/DoctorController.java
@@ -1,7 +1,18 @@
 package com.microservices.doctor.controller;
 
+import com.microservices.doctor.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DoctorController {
+    @Autowired
+    private DoctorService doctorService;
+
+    @GetMapping(path = "doctor/get-patient/{pName}")
+    public String getPname(@PathVariable("pName") String pName) {
+        return this.doctorService.getPatient(pName);
+    }
 }
--- clinics/doctor/src/main/java/com/microservices/doctor/service/DoctorService.java
+++ clinics/doctor/src/main/java/com/microservices/doctor/service/DoctorService.java
@@ -1,7 +1,12 @@
 package com.microservices.doctor.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class DoctorService {
+    public String getPatient(String patientName) {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject("http://localhost:8081/search/" + patientName, String.class);
+    }
 }
