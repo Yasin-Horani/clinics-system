c78e5592e21dd11104be8bd1f159371d9da3d249
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2026-02-15 19:48:16 +0100

    add env config

--- clinics/pay/src/main/java/com/microservices/pay/PayApplication.java
+++ clinics/pay/src/main/java/com/microservices/pay/PayApplication.java
@@ -1,5 +1,6 @@
 package com.microservices.pay;
 
+import com.microservices.pay.utilities.EnvLoaderUtility;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PayApplication {
 
 	public static void main(String[] args) {
+
+		EnvLoaderUtility envLoaderUtility = new EnvLoaderUtility();
+		envLoaderUtility.loadIntoSystemProperties();
 		SpringApplication.run(PayApplication.class, args);
 	}
 
