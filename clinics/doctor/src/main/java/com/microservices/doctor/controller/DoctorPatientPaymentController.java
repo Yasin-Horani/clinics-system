9a86b7664b16b1791cc7aa90f83a73afcaaa6205
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2026-02-20 13:27:22 +0100

    init payment controller in doctor service

---
+++ clinics/doctor/src/main/java/com/microservices/doctor/controller/DoctorPatientPaymentController.java
@@ -0,0 +1,27 @@
+package com.microservices.doctor.controller;
+
+import com.microservices.doctor.model.dto.payment.AddPaymentDTO;
+import com.microservices.doctor.model.dto.payment.PaymentDTO;
+import com.microservices.doctor.service.DoctorPatientPaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/doctor/")
+public class DoctorPatientPaymentController {
+
+    @Autowired
+    private DoctorPatientPaymentService doctorPatientPaymentService;
+
+    @GetMapping( "/payment/{transactionId}")
+    public PaymentDTO getPayment(@PathVariable UUID transactionId) {
+        return this.doctorPatientPaymentService.getPayment(transactionId);
+    }
+
+    @PostMapping(path = "/payment")
+    public PaymentDTO addPayment(@RequestBody AddPaymentDTO addPaymentDTO) {
+        return this.doctorPatientPaymentService.addPayment(addPaymentDTO);
+    }
+}
